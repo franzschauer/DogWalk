0f4ef1873e02287984ee678af8d41ed74e6aa3b6
unknown <gsullivan2016@02925pc.francisparker.org>
2015-04-28 12:58:01 -0700

    idk if this will work

---
+++ src/MainFrame.java
@@ -0,0 +1,43 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+
+
+public class MainFrame
+{
+	private static DogePanel tabby = new DogePanel();
+
+
+public MainFrame()
+{
+	
+}
+
+public static void main(String[] args)
+{
+	
+	
+//	JPanel panny = new JPanel(new BorderLayout());
+	/////
+	JFrame frame = new JFrame("DogWalk!");
+	JTextArea textPanel = new JTextArea("I'm a dog");
+	textPanel.setEditable(false);
+	
+	
+	
+}
+
+
